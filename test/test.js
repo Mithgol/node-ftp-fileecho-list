a8b0ebfd8d2eb0856e067d3855bfe5ae0f6dcac8
Mithgol the Webmaster <getgit@mithgol.ru>
2015-04-12 11:55:20 +0300

    add tests

---
+++ test/test.js
@@ -0,0 +1,32 @@
+/* global describe, it */
+var assert = require('assert');
+var path = require('path');
+var readerFTPFEcho = require('../');
+
+describe('Reader of lists of FTP-hosted Fidonet fechomail areas', function(){
+   it('can read two such lists correctly', function(done){
+      readerFTPFEcho(
+         [
+            path.join(__dirname, 'fido.hubahuba.su.ftpfecho'),
+            path.join(__dirname, 'fido.liona.ru.ftpfecho')
+         ],
+         function(err, fecholist){
+            if( err ) throw err;
+            assert.strictEqual(
+               fecholist.aftnged, 'ftp://fido.hubahuba.su/AFTNGED/'
+            );
+            assert.strictEqual(
+               fecholist.aftnbinkd, 'ftp://fido:fido@fido.liona.ru/aftnbinkd/'
+            );
+            assert.strictEqual(
+               fecholist.z2pnt, 'ftp://fido.hubahuba.su/Z2PNT/'
+            );
+            assert.strictEqual(
+               fecholist.xofchubslst,
+               'ftp://fido:fido@fido.liona.ru/xofchubslst/'
+            );
+            done();
+         }
+      );
+   });
+});
